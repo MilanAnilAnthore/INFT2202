fa36d4266bf57d92092bd7c476215f50687f32de
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2025-01-24 23:50:22 -0500

    ICE4 Final push

--- ice/client/app/animals/delete.js
+++
@@ -1,7 +0,0 @@
-/*
-    Name: Milan Anil Anthore
-    filename: delete.js
-    Course: INFT 2202
-    Date: January 10, 2025
-    Description: This is my delete script.
-*/
--- ice/client/app/animals/update.js
+++
@@ -1,7 +0,0 @@
-/*
-    Name: Milan Anil Anthore
-    filename: update.js
-    Course: INFT 2202
-    Date: January 10, 2025
-    Description: This is my update script.
-*/
