97ef3f901fde2fb4da37569ee67cdbad10eb1382
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2025-03-07 15:25:32 -0500

    ICE 10,11,12,14

---
+++ ice/Server/data/animal.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        "name": "Lion",
+        "species": "Panthera leo",
+        "habitat": "Savanna"
+    },
+    {
+        "name": "Elephant",
+        "species": "Loxodonta africana",
+        "habitat": "Forest"
+    },
+    // Add more animals as needed
+];
